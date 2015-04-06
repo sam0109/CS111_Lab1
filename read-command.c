83886ad903e6a4df9d8e569034d20e754c9eeece
Sam Sobell <sam.sobell@gmail.com>
2015-04-05 21:37:23 -0700

    partly done with implementing read

--- read-command.c
+++ read-command.c
@@ -13,13 +13,42 @@ struct command_node
     struct command_node* cursor;
 };
 
-
-typedef enum {WORD, OPEN_PAREN, CLOSE_PAREN, OPERATOR, UNKNOWN} word_type;
+struct stack
+{
+	struct stack* down;
+	command_t command;
+};
 
 typedef struct command_stream
 {
     
 };
+
+struct stack*
+pop(struct stack* head)
+{
+	struct stack* temp = head;
+	head = head->down;
+	return temp;
+}
+
+void
+push(struct stack* head, command_t new_command)
+{
+	struct stack* new_head = malloc(sizeof(struct stack));
+	new_head.command = new_command;
+	new_head->down = head;
+	head = new_head;
+}
+
+void
+push_new_command(struct stack* head, enum command_type in_command)
+{
+	struct command new_command = malloc(sizeof(struct command));
+	new_command.type = AND_COMMAND;
+	new_command.status = -1;
+	push(head, new_command);
+}
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *), void *get_next_byte_argument)
@@ -43,33 +72,89 @@ read_command_stream (command_stream_t s)
 }
 
 // a function to find the next word in the command
-char*
-scan_to_next_word(char* start, word_type *word)
+int
+scan_to_next_word(char* start, int beginning_of_next_word, enum word_type word) //return the number of characters in the next word (return 4 for "this")
+{																			// and find the beginning of the next word
+	return 0;
+}
+
+command_t
+pop_and_combine(command_t command, struct stack operators, struct stack commands)
 {
-	
+	return 0;
 }
 
 command_t
 generate_command_tree (char* input_string)
 {
-	word_type word;
-	char* beginning_of_next_word = input_string;
-	char* end_of_next_word = scan_to_next_word(input_string, *word);
-	while(end_of_next_word != NULL)
+	enum command_type word;
+	struct stack operators = malloc(sizeof(struct stack));
+	operators.command = 0;
+	struct stack commands = malloc(sizeof(struct stack));
+	commands.command = 0;
+	int beginning_of_next_word = 0;
+	int end_of_next_word = scan_to_next_word(input_string[beginning_of_next_word], &beginning_of_next_word, &word);
+	while(end_of_next_word != beginning_of_next_word)
 	{
 		switch(word)
 		{
-			case WORD:
+			case SIMPLE_COMMAND:
+				struct command new_command = malloc(sizeof(struct command));
+				new_command.type = SIMPLE_COMMAND;
+				new_command.status = -1;
+				int j = 0;
+				do
+				{
+					int word_len = end_of_next_word - beginning_of_next_word;
+					new_command.u->word[j] = malloc(word_len + 1);
+					for (int i = 0; i < word_len; i++)
+					{
+						new_command.u->word[j][i] = input_string[beginning_of_next_word + i];
+					}
+					new_command.u->word[j][word_len] = '\0';
+					j++;
+					end_of_next_word = scan_to_next_word(input_string[beginning_of_next_word], &beginning_of_next_word, &word);
+				}while(word == SIMPLE_COMMAND);
+				push(commands, new_command);
 				break;
-			case OPEN_PAREN:
+			case SUBSHELL_COMMAND:
+				if(input_string[beginning_of_next_word] == '(')
+				{
+					struct command new_command = malloc(sizeof(struct command));
+					new_command.type = SUBSHELL_COMMAND;
+					new_command.status = -1;
+					end_of_next_word = scan_to_next_word(input_string[beginning_of_next_word], &beginning_of_next_word, &word);
+				}
+				else
+				{
+					pop_and_combine();
+				}
 				break;
-			case CLOSE_PAREN:
+			case AND_COMMAND:
+				if(operators.command == 0)
+				{
+					push_new_command(operators, AND_COMMAND);
+				}
 				break;
-			case OPERATOR:
+			case SEQUENCE_COMMAND:
+				if(operators.command == 0)
+				{
+					push_new_command(operators, SEQUENCE_COMMAND);
+				}
 				break;
-			case UNKNOWN:
+			case OR_COMMAND:
+				if(operators.command == 0)
+				{
+					push_new_command(operators, OR_COMMAND);
+				}
 				break;
-			case defaut:
+			case PIPE_COMMAND:
+				if(operators.command == 0)
+				{
+					push_new_command(operators, PIPE_COMMAND);
+				}
+				break;
+			default:
 				break;
 		}
 		char* beginning_of_next_word = end_of_next_word;
